b18d3f2a76b1c91d3be90e818e6fcc9e7ad53e15
Nguyễn Hải Quang <haiquang9994@gmail.com>
2021-10-26 14:59:58 +0700

    update socket.js

---
+++ js/webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './src/socket.js',
+  output: {
+    filename: 'socket.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+}
