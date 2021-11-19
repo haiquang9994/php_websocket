b7c448202252d5710701e70b55a3a0877c18671d
Nguyễn Hải Quang <haiquang9994@gmail.com>
2021-11-19 14:37:28 +0700

    add client by ratchet/pawl

---
+++ src/Client.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace PHPWebsocket;
+
+use function Ratchet\Client\connect;
+
+class Client
+{
+    protected $address;
+
+    protected $port;
+
+    protected $secure;
+
+    public function __construct($address = '0.0.0.0', $port = 8080, $secure = false)
+    {
+        $this->address = $address;
+        $this->port = $port;
+        $this->secure = $secure;
+    }
+
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    public function getPort()
+    {
+        return $this->port;
+    }
+
+    public function getSecure()
+    {
+        return $this->secure;
+    }
+
+    public function send(string $eventName, array $data = [], callable $callback = null)
+    {
+        $url = sprintf("%s://%s:%s", $this->secure ? 'wss' : 'ws', $this->address, $this->port);
+        connect($url)->then(function ($conn) use ($eventName, $data, $callback) {
+            $conn->on('message', function ($msg) use ($callback, $conn) {
+                if (is_callable($callback)) {
+                    $data = json_decode($msg, true);
+                    if (count($data) === 2 && $data[0] === 0) {
+                        call_user_func($callback, $data[1]);
+                    }
+                    $conn->close();
+                }
+            });
+            $conn->send(json_encode([0, $eventName, $data]));
+            if (!is_callable($callback)) {
+                $conn->close();
+            }
+        }, function ($e) {
+            throw $e;
+        });
+    }
+}
